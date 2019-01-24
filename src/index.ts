77d52d97703aa1e0b685e034e338577e26224773
yuta nishimori <hunger_yn@icloud.com>
2019-01-25 06:08:37 +0900

    init canvas

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hello');
+// import * as fShader from './shaders/frag.glsl';
+// import * as vShader from './shaders/vert.glsl';
+
+const main = async (): Promise<void> => {
+  const width = 400;
+  const height = 300;
+  const canvas = document.createElement('canvas') as HTMLCanvasElement;
+  canvas.width = width;
+  canvas.height = height;
+  document.body.appendChild(canvas);
+
+  const gl = canvas.getContext('webgl')!;
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+window.addEventListener('load', main);
