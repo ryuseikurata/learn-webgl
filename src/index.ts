ad43521bb0a04756a15d51af83f648f6095e3cbe
yuta nishimori <hunger_yn@icloud.com>
2019-01-29 07:24:53 +0900

    display simple polygon

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-// import * as fShader from './shaders/frag.glsl';
-// import * as vShader from './shaders/vert.glsl';
+import Program from "./program";
+
+import fShader from './shaders/frag';
+import vShader from './shaders/vert';
 
 const main = async (): Promise<void> => {
   const width = 400;
@@ -12,6 +14,45 @@ const main = async (): Promise<void> => {
   const gl = canvas.getContext('webgl')!;
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
+
+  const program = new Program(gl, { fShaderSource: fShader, vShaderSource: vShader });
+  
+  const positionAttributeLocation = gl.getAttribLocation(program.glProgram, "a_position");
+  const positionBuffer = gl.createBuffer();
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  const positions = [
+    0, 0,
+    0, 0.5,
+    0.7, 0,
+  ];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  gl.useProgram(program.glProgram);
+
+  gl.enableVertexAttribArray(positionAttributeLocation);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  var size = 2;          // 2 components per iteration
+  var type = gl.FLOAT;   // the data is 32bit floats
+  var normalize = false; // don't normalize the data
+  var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+  var offset = 0;        // start at the beginning of the buffer
+  gl.vertexAttribPointer(
+      positionAttributeLocation, size, type, normalize, stride, offset);
+
+  // draw
+  var primitiveType = gl.TRIANGLES;
+  var offset = 0;
+  var count = 3;
+  gl.drawArrays(primitiveType, offset, count);
 }
 
 window.addEventListener('load', main);
